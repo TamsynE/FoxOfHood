63be5a5dbfdebe2fca5f0d6351cb3155e1d49819
Tamsyn Evezard <93171379+TamsynE@users.noreply.github.com>
2024-10-16 22:55:08 -0400

    Add files via upload

---
+++ Portfolio/server.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template
+import yfinance as yf
+
+from yahooquery import Screener
+
+app = Flask(__name__)
+
+@app.route('/')
+
+# @app.route('/login')
+# def login():
+#     return render_template("login.html", methods=['POST'])
+
+# @app.route('/register')
+# def register():
+#     return render_template("register.html", methods=['POST'])
+
+# @app.route('/admin')
+# def admin():
+#     return render_template("admin.html")
+
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/trade')
+def trade():
+    stocks = {}
+    # Fetch stock data 
+    with open('tickers.txt', 'r') as file:
+        tickers = [line.strip() for line in file.readlines()]
+
+    for ticker in tickers:
+        stock = yf.Ticker(ticker)
+        stock_info = stock.info
+        stocks[ticker] = {
+            "name": stock_info.get("shortName"),
+            "price": stock_info.get("currentPrice")
+        }
+
+    return render_template("trade.html", stocks=stocks)
+
+# Log route
+@app.route('/log')
+def log():
+    return render_template("log.html")
+
+@app.route('/logout')
+def logout():
+    return render_template("logout.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
